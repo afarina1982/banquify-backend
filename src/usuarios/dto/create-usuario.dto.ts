a92ef5d2db2e66b1a7e423bd71fe1ec9867ea451
Andres Fariña <farinavaldivia@gmail.com>
2024-08-21 16:05:53 -0400

    Módulo usuarios 1

--- src/usuarios/dto/create-usuario.dto.ts
+++ src/usuarios/dto/create-usuario.dto.ts
@@ -1,1 +1,11 @@
-export class CreateUsuarioDto {}
+import { ApiProperty } from "@nestjs/swagger";
+
+export class CreateUsuarioDto {
+    @ApiProperty({ default: 'Su Nombre' })
+    public nombre: string; // nombre del usuario
+    @ApiProperty({ default: 'Su Correo Electrónico' })
+    public correoElectronico: string; // correo electrónico del usuario
+    @ApiProperty({ default: '12345' })
+    public contrasena: string; // contraseña del usuario
+    
+}
--- src/usuarios/entities/usuario.entity.ts
+++ src/usuarios/entities/usuario.entity.ts
@@ -1,1 +1,19 @@
-export class Usuario {}
+export class Usuario {
+    public id: number; // identificador numérico único, generado automáticamente
+    public nombre: string; // nombre del usuario
+    public correoElectronico: string; // correo electrónico del usuario
+    public contrasena: string; // contraseña del usuario
+    public puntosAcumulados: number; // número de puntos de recompensa acumulados
+
+    constructor(id: number, nombre: string, correoElectronico: string, contrasena: string) {
+        this.id = id;
+        this.nombre = nombre;
+        this.correoElectronico = correoElectronico;
+        this.contrasena = contrasena;
+        this.puntosAcumulados = 0;
+    }
+    
+}
+
+
+
